9700fae1138ac843b96a4785cf5156f4cd072a89
RamonGiovane <ramon.giovane@inovamind.com.br>
2021-11-13 18:58:09 -0300

    17: Propriedades sao readonly

--- src/components/basicos/ComponenteComParametro.jsx
+++ src/components/basicos/ComponenteComParametro.jsx
@@ -2,12 +2,13 @@ import React from "react"
 
 export default function(props){
   const status = props.nota >= 7 ? 'Aprovado' : 'Reprovado'
-
+  // props.nota = Math.ceil(props.nota) -> dispara erro. Propriedades sao readonly!!
+  const arredondada = Math.ceil(props.nota) // funciona!
   return(
     <div>
         <h2>{ props.titulo }</h2>
         <p>
-          { props.aluno } tem nota { props.nota } e foi <strong>{ status }</strong>
+          { props.aluno } tem nota { arredondada } e foi <strong>{ status }</strong>
         </p> 
     </div>
   )
